39031e4224087b0c8fa5f084f0345159903a93e1
Daniel CM <daniel_20589@hotmail.com>
2021-11-15 21:58:15 -0600

    word validation implemented

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
 nato_data_frame = pandas.DataFrame(data)
@@ -30,10 +10,17 @@ nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()
 print(nato_dict)
 
 
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Type the word: ").upper()
+def generate_phonetic():
+    word = input("Type the word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-output_list = [nato_dict[letter] for letter in word]
-print(output_list)
 
+generate_phonetic()
